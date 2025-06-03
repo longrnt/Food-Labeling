7dd1c392d928ff6b0c5e0a4d4a414dd7d68fd9b8
Long Tran <LTran@HAEAus.com>
2025-06-03 11:47:16 -0700

    [back-end] change label list in food entity to label set

--- src/main/java/com/food/labeling/payload/FoodDTO.java
+++ src/main/java/com/food/labeling/payload/FoodDTO.java
@@ -3,12 +3,12 @@ package com.food.labeling.payload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 public class FoodDTO {
     private Long foodId;
     private String foodName;
-    private List<String> labels;
+    private Set<String> labels;
 }
--- src/main/java/com/food/labeling/service/FoodServiceImpl.java
+++ src/main/java/com/food/labeling/service/FoodServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FoodServiceImpl implements FoodService {
@@ -51,7 +52,7 @@ public class FoodServiceImpl implements FoodService {
                 .map(f -> new FoodDTO(
                         f.getFoodId(),
                         f.getFoodName(),
-                        f.getLabels().stream().map(Label::getLabelName).toList()
+                        f.getLabels().stream().map(Label::getLabelName).collect(Collectors.toSet())
                 ))
                 .toList();
 
